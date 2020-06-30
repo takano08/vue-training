ae04d3d9e758e08589a21472c37e1eb5bd64ed88
takano <ri1saxxminny83xx@gmail.com>
2020-06-30 23:39:50 +0900

    view-summary

--- hello/js/main.js
+++ hello/js/main.js
@@ -1,9 +1,19 @@
-Vue.component('hello-component',{
-    template:'<p>Hello</p>'
-})
-
 new Vue({
     el:'#app',
-    
+    data: {
+        message: 'Hello World!',
+        url: 'https://jp.vuejs.org/',
+        toggle:true,
+        languages: [
+            'Javascript',
+          'Ruby',
+          'Python'
+        ]
+    },
+    methods: {
+        clickHandler:function(){
+            this.message='Clicked!'
+        }
+    }
     
 })
