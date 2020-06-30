772fa663878e9d9fb2c70c8c22517bdfd6fe41b0
takano <ri1saxxminny83xx@gmail.com>
2020-06-30 17:09:31 +0900

    v-on event

--- hello/js/main.js
+++ hello/js/main.js
@@ -1,11 +1,11 @@
 new Vue({
     el:'#app',
     data: {
-        colors:['Red','Green','Blue'],
-        user: {
-            firstName: 'Taro',
-            lastName: 'Yamada',
-            age: 28
+        now: ''
+    },
+    methods: {
+        onclick: function() {
+            this.now =new Date().toLocaleString();
         }
     }
 })
