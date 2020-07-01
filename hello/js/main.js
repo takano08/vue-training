5410e540415ff6ba2553eeb5772c1eb32f01b716
takano <ri1saxxminny83xx@gmail.com>
2020-07-01 14:56:09 +0900

    bitcoin-price

--- hello/js/main.js
+++ hello/js/main.js
@@ -1,23 +1,29 @@
 new Vue({
     el:'#app',
     data:{
-        newItem: '',
-        todos: []
+        bpi: null,
+        hasError: false,
+        loading: true
     },
-    methods: {
-        addItem:function(event){
-           if(this.newItem=='') return;
-            
-           var todo={
-               item: this.newItem,
-               isDone: false
-           };
-           this.todos.push(todo);
-           this.newItem='';
-        },
-        deleteItem: function(index){
-            this.todos.splice(index, 1)
+    mounted: function(){
+      axios.get('https://api.coindesk.com/v1/bpi/currentprice.json') 
+      .then(function(response){
+          
+          this.bpi=response.data.bpi
+      }.bind(this)) 
+      .catch(function(error){
+          console.log(error);
+          this.hasError=true
+      }.bind(this))
+      .finally(function(){
+          this.loading=false
+      }.bind(this))
+    },
+    filters: {
+        currencyDecimal(value){
+            return value.toFixed(2)
         }
     }
+    
 
 })
