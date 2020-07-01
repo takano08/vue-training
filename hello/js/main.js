faa2f09928566b13e03a30eeff206fa304b07f8a
takano <ri1saxxminny83xx@gmail.com>
2020-07-01 20:32:32 +0900

    joint-filter

--- hello/js/main.js
+++ hello/js/main.js
@@ -1,14 +1,14 @@
-Vue.filter('numberFormat',function(value){
+Vue.filter('toUSD', function(jpy){
+    return jpy / 100
+})
+Vue.filter('numberFormat', function(value){
     return value.toLocaleString()
 })
+
 new Vue({
     el:'#app',
     data: {
-        price: 298000000
-    },
-    /*filters: {
-        numberFormat: function(value){
-            return value.toLocaleString()
-        }
-    } */
+        jpyPrice: 298000
+    }
+    
 })
