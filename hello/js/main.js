4857373d6508df8eaa0bceaa02ba24e9d4ab358b
takano <ri1saxxminny83xx@gmail.com>
2020-07-01 13:14:31 +0900

    todo-list

--- hello/js/main.js
+++ hello/js/main.js
@@ -1,19 +1,23 @@
 new Vue({
     el:'#app',
-    data: {
-        message: 'Hello World!',
-        url: 'https://jp.vuejs.org/',
-        toggle:true,
-        languages: [
-            'Javascript',
-          'Ruby',
-          'Python'
-        ]
+    data:{
+        newItem: '',
+        todos: []
     },
     methods: {
-        clickHandler:function(){
-            this.message='Clicked!'
+        addItem:function(event){
+           if(this.newItem=='') return;
+            
+           var todo={
+               item: this.newItem,
+               isDone: false
+           };
+           this.todos.push(todo);
+           this.newItem='';
+        },
+        deleteItem: function(index){
+            this.todos.splice(index, 1)
         }
     }
-    
+
 })
