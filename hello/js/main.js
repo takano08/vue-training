41ed174c63553618d2e8f1a9b9205a639f3ab0ae
takano <ri1saxxminny83xx@gmail.com>
2020-07-01 15:59:31 +0900

    v-pre

--- hello/js/main.js
+++ hello/js/main.js
@@ -2,10 +2,6 @@ new Vue({
     el:'#app',
     data: {
         message: 'Hello Vue.js!'
-    },
-    methods: {
-        clickHandler: function(event){
-            this.message=this.message.split('').reverse().join('')
-        }
     }
+    
 })
