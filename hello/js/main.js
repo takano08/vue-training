05ce791cac7af50d8a7322a2364473b4710c7190
takano <ri1saxxminny83xx@gmail.com>
2020-07-02 11:03:53 +0900

    computed-cache

--- hello/js/main.js
+++ hello/js/main.js
@@ -2,17 +2,16 @@
 
 new Vue({
     el:'#app',
-    data: {
-       basePrice: 100
-    },
     computed: {
-        taxIncludedPrice: {
-            get: function() {
-                return parseInt(this.basePrice * 1.08)
-            },
-            set: function(taxIncludedPrice) {
-                this.basePrice =Math.ceil(taxIncludedPrice/1.08)
-            }
+        computedNumber: function(){
+            console.log('computed!');
+            return Math.random()
+        }
+    },
+    methods: {
+        methodsNumber: function(){
+            console.log('methods!');
+            return Math.random()
         }
     }
     
