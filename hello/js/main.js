b98191599bbd2829cf92791289b1236cb365b0dd
takano <ri1saxxminny83xx@gmail.com>
2020-07-06 21:23:44 +0900

    realtime-search

--- hello/js/main.js
+++ hello/js/main.js
@@ -3,28 +3,43 @@
 new Vue({
     el:'#app',
     data: {
-        colors: [
-            { name: 'Red'},
-            { name: 'Green'},
-            { name: 'Blue'}
-        ]
+      items: null,
+      keyword: '',
+      message: ''
     },
     watch: {
-        colors: {
-            handler: function(newValue, oldValue){
-                console.log('Update!');
-                console.log('new: %s, oldValue: %s',
-                JSON.stringify(newValue, null, '\t'),
-                JSON.stringify(oldValue, null, '\t'))
-            },
-            deep: true,
-            immediate: true
-
-        }
-    
-    
-        
-
+      keyword: function(newKeyword , oldKeyword){
+          //console.log(newKeyword);
+          this.message = 'Waiting for you to stop typing...' 
+          this.debouncedGetAnswer()
+      }
+    },
+    created: function() {
+        //this.keyword = 'JavaScript'
+        //this.getAnswer()
+        this.debouncedGetAnswer = _.debounce(this.getAnswer, 1000)
+    },
+    methods: {
+      getAnswer: function() {
+          if(this.keyword === '') {
+              this.items = null
+              this.message=''
+              return
+          }
+            this.message = 'Loading...'
+            var vm = this
+            var params = { page: 1, per_page: 20, query: this.keyword}
+            axios.get('https://qiita.com/api/v2/items',{params})
+             .then(function(response){
+                 console.log(response)
+                 vm.items =response.data
+             })
+             .catch(function(error){
+                 vm.message = 'Error!' + error
+             })
+             .finally(function(){
+                 vm.message =''
+             })
+      }
     }
-    
 })
