23aaefa933089c8491c7f1a67b904b8600bd278e
takano <ri1saxxminny83xx@gmail.com>
2020-07-06 12:17:52 +0900

    watcher-base2

--- hello/js/main.js
+++ hello/js/main.js
@@ -3,12 +3,27 @@
 new Vue({
     el:'#app',
     data: {
-        message: 'hello Vue.js'
+        km: 0,
+        m: 0,
+        mm: 0,
     },
     watch: {
-        message: function(newValue, oldVlue) {
-            console.log('new:%s, old:%s',newValue,oldVlue);
+        km: function(value) {
+            this.km = value
+            this.m = value * 1000
+            this.mm = value * 1000000
+        },
+        m: function(value) {
+            this.km = value / 1000
+            this.m = value
+            this.mm = value *1000
+        },
+        mm: function(value) {
+            this.km =value /1000000
+            this.m = value /1000
+            this.mm = value
         }
+
     }
     
 })
