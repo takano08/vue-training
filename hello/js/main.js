a05e7492bfc04d48dda2fb8f33090bb4b48016b3
takano <ri1saxxminny83xx@gmail.com>
2020-07-06 13:40:45 +0900

    deep-watch2

--- hello/js/main.js
+++ hello/js/main.js
@@ -4,7 +4,7 @@ new Vue({
     el:'#app',
     data: {
         colors: [
-            { name: 'RED'},
+            { name: 'Red'},
             { name: 'Green'},
             { name: 'Blue'}
         ]
@@ -13,6 +13,9 @@ new Vue({
         colors: {
             handler: function(newValue, oldValue){
                 console.log('Update!');
+                console.log('new: %s, oldValue: %s',
+                JSON.stringify(newValue, null, '\t'),
+                JSON.stringify(oldValue, null, '\t'))
             },
             deep: true
 
