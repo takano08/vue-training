beaaedb2c49ce446e9c9b42e3f3f7c7f67204806
takano <ri1saxxminny83xx@gmail.com>
2020-07-06 23:19:04 +0900

    object-data

--- hello/js/main.js
+++ hello/js/main.js
@@ -1,8 +1,10 @@
 new Vue({
     el:'#app',
     data: {
-        largeClass: 'large',
-        dangerClass: 'text-danger'
+        classObject: {
+            large: true,
+            'text-danger' : true
+        }
         
     }
 })
