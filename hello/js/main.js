5d643a89abb8420595b9264956eaf465aba98944
takano <ri1saxxminny83xx@gmail.com>
2020-07-06 12:36:26 +0900

    watch

--- hello/js/main.js
+++ hello/js/main.js
@@ -3,26 +3,18 @@
 new Vue({
     el:'#app',
     data: {
-        km: 0,
-        m: 0,
-        mm: 0,
+        firstName: '',
+        lastName: '',
+        fullName: '',
     },
     watch: {
-        km: function(value) {
-            this.km = value
-            this.m = value * 1000
-            this.mm = value * 1000000
+        firstName: function(value) {
+            this.fullName = value + ' ' + this.lastName    
         },
-        m: function(value) {
-            this.km = value / 1000
-            this.m = value
-            this.mm = value *1000
-        },
-        mm: function(value) {
-            this.km =value /1000000
-            this.m = value /1000
-            this.mm = value
+        lastName: function(value) {
+            this.fullName = this.firstName + ' ' +value
         }
+        
 
     }
     
