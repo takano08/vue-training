d494ce4c29912f6a73aad8489f84ba81e972daf3
takano <ri1saxxminny83xx@gmail.com>
2020-07-10 17:18:24 +0900

    component-option

--- hello/js/main.js
+++ hello/js/main.js
@@ -1,11 +1,14 @@
-var helloComponent = {
-    template: '<p>Hello</p>'
-}
+Vue.component('button-counter',{
+    data: function() {
+        return{
+            count: 0
+        }
+    },
+    template: '<div><span>count: </span><button v-on:click="count++">{{ count }}</button></div>'
+})
 
 new Vue({
     el:'#app',
-    components: {
-        'hello-component' :helloComponent
-    }
+    
     
 })
