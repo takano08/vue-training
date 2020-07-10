34e848a27b994590186b449a8ef919f0c4125044
takano <ri1saxxminny83xx@gmail.com>
2020-07-10 17:55:51 +0900

    transition

--- hello/js/main.js
+++ hello/js/main.js
@@ -1,14 +1,10 @@
-Vue.component('button-counter',{
-    data: function() {
-        return{
-            count: 0
-        }
-    },
-    template: '<div><span>count: </span><button v-on:click="count++">{{ count }}</button></div>'
-})
+
 
 new Vue({
     el:'#app',
+    data: {
+        show: false
+    }
     
     
 })
