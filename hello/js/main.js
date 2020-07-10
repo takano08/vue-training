79289b312676b5502893047b915bf138510487e3
takano <ri1saxxminny83xx@gmail.com>
2020-07-10 13:36:19 +0900

    component-local

--- hello/js/main.js
+++ hello/js/main.js
@@ -1,8 +1,11 @@
-Vue.component('hello-component',{
-    template:'<p>Hello</p>'
-})
+var helloComponent = {
+    template: '<p>Hello</p>'
+}
 
 new Vue({
     el:'#app',
+    components: {
+        'hello-component' :helloComponent
+    }
     
 })
